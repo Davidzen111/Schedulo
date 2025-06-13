afe1ee911122656f23ddfd88788ae08caa63ca45
Davidzen <davidzen662@gmail.com>
2025-06-13 21:19:22 +0800

    feat: add BaseModel

--- ProjectManagementApp/src/model/BaseModel.java
+++ ProjectManagementApp/src/model/BaseModel.java
@@ -1,1 +1,17 @@
+package model;
 
+public abstract class BaseModel {
+    protected String id;
+
+    public BaseModel() {}
+
+    public BaseModel(String id) {
+        this.id = id;
+    }
+
+    public String getId() { return id; }
+
+    public void setId(String id) { this.id = id; }
+
+    public abstract String toString(); // Biarkan subclass override
+}
